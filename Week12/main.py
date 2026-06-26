bdefa1498cff4c98f34e6809093e44ecebdef8ee
SandaliR329 <sandalirj@gmail.com>
2026-06-27 10:05:33 +1200

    Activity 2

---
+++ Week12/main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Flask HTML Activity</title>
+    </head>
+    <body>
+        <h1>Welcome to Flask!</h1>
+
+        <p>This is my first HTML page using Flask.</p>
+
+        <p>
+            Learn Flask by visiting the
+            <a href="https://flask.palletsprojects.com/en/stable/quickstart/" target="_blank">
+                Flask Quickstart Documentation
+            </a>.
+        </p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
