6b2dbc74cd1ae12835c73076b043f3bf18dee553
SandaliR329 <sandalirj@gmail.com>
2026-06-27 08:59:20 +1200

    Activity 1.3: Flask

--- Week12/HelloFlask/app.py
+++ Week12/HelloFlask/app.py
@@ -10,5 +10,9 @@ def hello_flask():
 def bye():
     return "<p>Bye, Flask!</p>"
 
+@app.route("/username/<name>")
+def learn(name):
+    return f"{name} is learning Flask!"
+
 if __name__ == "__main__":
     app.run(debug=True)
